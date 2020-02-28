6e773d34989edb472d206d4567741cbd50b943b0
WMXPY <wm@mengw.io>
2020-02-28 11:41:52 -0600

    init unit tests

---
+++ test/number/deconstruction_test.dart
@@ -0,0 +1,20 @@
+import 'package:numeric/numeric.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Number Deconstruction', () {
+    test('get alias can return word', () {
+      final int target = 100;
+
+      final List<int> result = evenlySeprateInteger(target, 5);
+      expect(result, equals([20, 20, 20, 20, 20]));
+    });
+
+    test('get alias can return word - difference', () {
+      final int target = 104;
+
+      final List<int> result = evenlySeprateInteger(target, 5);
+      expect(result, equals([21, 21, 21, 21, 20]));
+    });
+  });
+}
