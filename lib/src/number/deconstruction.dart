debc784b53d31216ead8b5da4db05185a7bee571
WMXPY <wm@mengw.io>
2020-02-28 11:36:40 -0600

    init numeric

---
+++ lib/src/number/deconstruction.dart
@@ -0,0 +1,12 @@
+List<int> evenlySeprateInteger(int target, int length) {
+  final int unit = (target / length).floor();
+  final int consumed = unit * length;
+  final int difference = target - consumed;
+
+  final List<int> result = [];
+  for (int i = 0; i < length; i++) {
+    final bool shouldAdd = i < difference;
+    result.add(shouldAdd ? unit + 1 : unit);
+  }
+  return result;
+}
