502345fc4e6d94e98c7c549acdb275164d25fadc
WMXPY <wm@mengw.io>
2020-05-08 11:41:01 -0500

    test attempt

--- test/list/random_test.dart
+++ test/list/random_test.dart
@@ -1,5 +1,11 @@
+import 'package:numeric/numeric.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('List Random', () {});
+  group('List Random', () {
+    test('get random item of list', () {
+      final int result = randomElementOfList([1]);
+      expect(result, equals(1));
+    });
+  });
 }
