3fe9583e0f05c2eb66fa80a68054b1f590d83c66
WMXPY <wm@mengw.io>
2020-05-09 14:28:46 -0500

    add exports

--- lib/numeric.dart
+++ lib/numeric.dart
@@ -1,5 +1,7 @@
 library numeric;
 
+export "./src/dynamic/parse.dart";
 export "./src/list/random.dart";
 export "./src/number/deconstruction.dart";
 export "./src/number/random.dart";
+export "./src/string/parse.dart";
