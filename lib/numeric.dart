ce8d20ee44305e36dd863133e3898a7185204615
WMXPY <wm@mengw.io>
2020-05-11 14:12:32 -0500

    fix export

--- lib/numeric.dart
+++ lib/numeric.dart
@@ -1,7 +1,10 @@
 library numeric;
 
 export "./src/dynamic/parse.dart";
+export "./src/entity/declare.dart";
+export "./src/find/list.dart";
 export "./src/list/random.dart";
 export "./src/number/deconstruction.dart";
 export "./src/number/random.dart";
 export "./src/string/parse.dart";
+export "./src/util.dart";
